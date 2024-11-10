91dbf09496d525c7330e6974f1b159c9c80ae935
stardvst <lilia.manukyan@hotmail.com>
2024-11-10 21:50:02 +0400

    hero section

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,6 +2,11 @@ export default function Home() {
   console.log("WHEN AM I?");
 
   return (
-    <h1 className="text-3xl">Home</h1>
+    <>
+      <section className="pink_container">
+        <h1 className="heading">Pitch your startup, <br /> Connect With Enterpreneurs</h1>
+        <p className="sub-heading !max-w-3xl">Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competitions.</p>
+      </section>
+    </>
   );
 }
